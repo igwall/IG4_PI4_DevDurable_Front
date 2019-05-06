f095f1e451159282a8c9d87a643e8bb8fc4d067c
Alexis559 <alexis559.sanchez@gmail.com>
2019-05-06 11:18:02 +0200

    [DESIGN] Trajets design.

--- components/trajet/Trajets.js
+++ components/trajet/Trajets.js
@@ -2,6 +2,9 @@ import React from 'react'
 import {View, Text, FlatList, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native'
 import {Ionicons} from '@expo/vector-icons';
 import Style from "../../styles/Style";
+import Actu from './ActuTrajet'
+import Covoiturage from './Covoiturage'
+import TrajetsEcologiques from './TrajetsEcologiques'
 
 export default class Trajets extends React.Component {
 
@@ -12,42 +15,15 @@ export default class Trajets extends React.Component {
         }
     }
 
-    renderItem = data =>
-        <TouchableOpacity style={styles.row}>
-           <Text style={{color: Style.white, fontWeight: 'bold'}}>{data.item.key}</Text>
-        </TouchableOpacity>
-
-    renderItem2 = data =>
-        <TouchableOpacity style={styles.row2}>
-            <Text style={{color: Style.white, fontWeight: 'bold'}}>{data.item.key}</Text>
-        </TouchableOpacity>
-
     render() {
         return (
-            <SafeAreaView style={{flex: 1}}>
+            <SafeAreaView style={{flex: 1, backgroundColor: Style.darkblue}}>
                 <View style={{flex: 1}}>
-                        <FlatList
-                            contentContainerStyle={styles.list2}
-                            data={[
-                                {key: 'Trajet1'},
-                            ]}
-                            renderItem={item => this.renderItem2(item)}
-                            style={{flex: 1}}
-                        />
-                        <FlatList
-                            contentContainerStyle={styles.list}
-                            data={[
-                                {key: 'Trajet1'},
-                                {key: 'Trajet2'},
-                                {key: 'Trajet3'},
-                                {key: 'Trajet4'},
-                                {key: 'Trajet5'},
-                                {key: 'Trajet6'},
-                                {key: 'Trajet7'},
-                                {key: 'Trajet8'},
-                            ]}
-                            renderItem={item => this.renderItem(item)}
-                        />
+                    <Actu/>
+                    <Text style={styles.labelList}>Trajets écologiques</Text>
+                    <TrajetsEcologiques/>
+                    <Text style={styles.labelList}>Covoiturage</Text>
+                    <Covoiturage/>
                 </View>
             </SafeAreaView>
         );
@@ -55,29 +31,9 @@ export default class Trajets extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    list: {
-        flexDirection: 'row',
-    },
-    list2: {
-        flex: 1,
-        flexDirection: 'column',
-    },
-    row: {
-        backgroundColor: Style.darkpurple,
-        borderRadius: 10,
-        width: 125,
-        height: 125,
-        marginHorizontal: 10,
-        marginTop: 5,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    row2: {
-        backgroundColor: Style.bluegreen,
-        borderRadius: 10,
-        height: 125,
-        marginHorizontal: 10,
-        marginTop: 5,
-        alignItems: 'center',
+    labelList: {
+        color: Style.white,
+        fontWeight: 'bold',
+        marginLeft: 10
     }
 });
---
+++ components/trajet/TrajetsEcologiques.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import Style from "../../styles/Style";
+
+export default class Covoiturage extends React.Component {
+
+    renderItem = data =>
+        <TouchableOpacity style={styles.row}>
+            <Text style={{color: Style.bluegreen, fontWeight: 'bold'}}>{data.item.key}</Text>
+        </TouchableOpacity>
+
+
+    render() {
+        return (
+            <FlatList
+                contentContainerStyle={styles.list}
+                data={[
+                    {key: 'Trajet1'},
+                    {key: 'Trajet2'},
+                    {key: 'Trajet3'},
+                    {key: 'Trajet4'},
+                    {key: 'Trajet5'},
+                    {key: 'Trajet6'},
+                    {key: 'Trajet7'},
+                    {key: 'Trajet8'},
+                ]}
+                renderItem={item => this.renderItem(item)}
+            />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    row: {
+        backgroundColor: Style.white,
+        borderRadius: 10,
+        width: 125,
+        height: 125,
+        marginHorizontal: 10,
+        marginTop: 5,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
